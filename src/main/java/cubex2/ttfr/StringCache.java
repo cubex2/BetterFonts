8bccbd6fece4724d4d0e3af53ddc93f04bcd387b
unknown <firebeast-breuer@web.de>
2016-04-11 17:49:21 +0200

    Fixed font turns black after an entity has been rendered hurt.

--- src/main/java/cubex2/ttfr/StringCache.java
+++ src/main/java/cubex2/ttfr/StringCache.java
@@ -392,6 +392,9 @@ public class StringCache
             return 0;
         }
 
+        // Fix for what RenderLivingBase#setBrightness does
+        GL11.glTexEnvi(GL11.GL_TEXTURE_ENV, GL11.GL_TEXTURE_ENV_MODE, GL11.GL_MODULATE);
+
         /* Make sure the entire string is cached before rendering and return its glyph representation */
         Entry entry = cacheString(str);
 
