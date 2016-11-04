7de52273da309b6cb25fed61707634afa6cba860
cubex2 <codemaster44@web.de>
2016-11-04 11:49:13 +0100

    Fixed incompatibility with optifine.

--- src/main/java/cubex2/ttfr/BFDummyContainer.java
+++ src/main/java/cubex2/ttfr/BFDummyContainer.java
@@ -20,7 +20,7 @@ public class BFDummyContainer extends DummyModContainer
         ModMetadata meta = getMetadata();
         meta.modId = "BetterFonts";
         meta.name = "BetterFonts";
-        meta.version = "1.2.3";
+        meta.version = "1.2.4";
         meta.credits = "thvortex for original codes, iSuzutsuki";
         meta.authorList = Arrays.asList("CubeX2");
         meta.description = "";
